e27fae77774e2ecf7f017678caf4b370f360346e
tahersb <tahersb@gmail.com>
2015-05-15 17:08:30 +0530

    Enabled wrapUp() method in finally blcok

--- src/main/java/net/continuumsecurity/jbehave/StoryRunner.java
+++ src/main/java/net/continuumsecurity/jbehave/StoryRunner.java
@@ -112,7 +112,7 @@ public class StoryRunner extends JUnitStoryRunner {
             log.debug("Caught exception from StoryRunner.execute()");
             e.printStackTrace();
         } finally {
-            wrapUp();
+            new WrapUpScanSteps.wrapUp();
         }
         System.exit(0);
     }
